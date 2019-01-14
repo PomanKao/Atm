6692f8541fd8c150284017d5f353c5032728362a
Poman <ayujolin520@gmail.com>
2019-01-14 12:42:34 +0800

    Initial commit

---
+++ app/src/main/java/com/poman/atm/LoginActivity.java
@@ -0,0 +1,63 @@
+package com.poman.atm;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private  EditText edUserid;
+    private  EditText edPasswd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        edUserid = findViewById(R.id.userid);
+        edPasswd = findViewById(R.id.passwd);
+    }
+
+    public void login(View view) {
+        String userid = edUserid.getText().toString();
+        final String passwd = edPasswd.getText().toString();
+        FirebaseDatabase.getInstance().getReference("users").child(userid).child("password")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String pw = (String) dataSnapshot.getValue();
+                        if (pw.equals(passwd)) {
+                            setResult(RESULT_OK);
+                            finish();
+                        } else {
+                            new AlertDialog.Builder(LoginActivity.this)
+                                    .setTitle("登入失敗")
+                                    .setMessage("請檢察帳號密碼")
+                                    .setPositiveButton("OK",null)
+                                    .show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+       /* if ("jack".equals(userid) && "1234".equals(passwd)) {
+            setResult(RESULT_OK);
+            finish();
+        }*/
+    }
+
+    public void quit(View vIew) {
+
+    }
+
+}
