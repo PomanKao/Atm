e61d5d177d446ff20b3e05806ce1ec442d890ff8
Poman <ayujolin520@gmail.com>
2019-01-15 18:54:53 +0800

    test RecyclerView with a custom adapter and ViewHolder

--- app/src/main/java/com/poman/atm/MainActivity.java
+++ app/src/main/java/com/poman/atm/MainActivity.java
@@ -2,22 +2,33 @@ package com.poman.atm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_LOGIN = 100;
+    private static final String TAG = MainActivity.class.getSimpleName();
     boolean logon = false;
-    String[] functions = null;
+    private List<Function> functions;
+    //    String[] functions = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,12 +52,81 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         // Recycler
+        setupFunctions();
+
         RecyclerView recyclerView = findViewById(R.id.recycler);
         recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this,3));
+
         //Adapter
-        FunctionAdapter adapter = new FunctionAdapter(this);
+//        FunctionAdapter adapter = new FunctionAdapter(this);
+        IconAdapter adapter = new IconAdapter();
         recyclerView.setAdapter(adapter);
+    }
+
+    private void setupFunctions() {
+        functions = new ArrayList<>();
+        String[] funcs = getResources().getStringArray(R.array.functions);
+        functions.add(new Function(funcs[0], R.drawable.func_transaction));
+        functions.add(new Function(funcs[1], R.drawable.func_balance));
+        functions.add(new Function(funcs[2], R.drawable.func_finance));
+        functions.add(new Function(funcs[3], R.drawable.func_contacts));
+        functions.add(new Function(funcs[4], R.drawable.func_exit));
+    }
+
+    public class IconAdapter extends RecyclerView.Adapter<IconAdapter.IconViewHolder> {
+        @NonNull
+        @Override
+        public IconViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+            View view = getLayoutInflater().inflate(R.layout.item_icon, parent, false);
+            return new IconViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull IconViewHolder holder, int position) {
+            final Function fun = functions.get(position);
+            holder.nameText.setText(fun.getName());
+            holder.iconImage.setImageResource(fun.getIcon());
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemClicked(fun);
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return functions.size();
+        }
+
+        public class IconViewHolder extends RecyclerView.ViewHolder {
+            private final TextView nameText;
+            private final ImageView iconImage;
+            public IconViewHolder(@NonNull View itemView) {
+                super(itemView);
+                nameText = itemView.findViewById(R.id.item_name);
+                iconImage = itemView.findViewById(R.id.item_icon);
+            }
+        }
+    }
+
+    private void itemClicked(Function fun) {
+        Log.d(TAG, "itemClicked: " + fun.getName());
+        switch (fun.getIcon()) {
+            case R.drawable.func_transaction:
+                break;
+            case R.drawable.func_balance:
+                break;
+            case R.drawable.func_finance:
+                break;
+            case R.drawable.func_contacts:
+                break;
+            case R.drawable.func_exit:
+                finish();
+                break;
+        }
     }
 
     @Override
